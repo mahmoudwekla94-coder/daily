71f7e57d6da0eae2f39cf71dcf4d5e0b66a2cfd9
mahmoudwekla94-coder <mahmoudwekla94@gmail.com>
2026-06-01 15:17:12 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -45,33 +45,32 @@ module.exports = async function webhook(req, res) {
     // =========================
     // Store Config
     // =========================
-  const storeConfig = {
+const storeConfig = {
   EQ: {
-    template: "confirmation_order",
+    template: "confirmation",
     lang: "en",
     currency: "ريال سعودي",
     defaultCountry: "KSA",
   },
   BZ: {
-    template: "confirmation_order",
+    template: "confirmation",
     lang: "en",
     currency: "ريال سعودي",
     defaultCountry: "KSA",
   },
   GZ: {
-    template: "confirmation_order",
+    template: "confirmation",
     lang: "en",
     currency: "ريال سعودي",
     defaultCountry: "KSA",
   },
   SH: {
-    template: "confirmation_order",
+    template: "confirmation",
     lang: "en",
     currency: "ريال سعودي",
     defaultCountry: "KSA",
   },
 };
-
     const cfg = storeConfig[storeTag] || storeConfig.EQ;
 
     // =========================
